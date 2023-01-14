4fff8e3ea5b16424901f71423cac436bf52b6648
Venkataswethareddy <swethasreenu555@gmail.com>
2023-01-14 10:30:00 +0530

    feat: add audio asset path placeholders

---
+++ lib/core/constants/asset_paths.dart
@@ -0,0 +1,16 @@
+/// Placeholder paths for audio assets. Plug in real files later.
+class AssetPaths {
+  AssetPaths._();
+
+  // ── Audio (placeholder paths — add .mp3 files to assets/audio/) ──
+  static const String audioHappy = 'assets/audio/happy_purr.mp3';
+  static const String audioSad = 'assets/audio/sad_whimper.mp3';
+  static const String audioEat = 'assets/audio/eating_crunch.mp3';
+  static const String audioSleep = 'assets/audio/sleeping_snore.mp3';
+  static const String audioEvolve = 'assets/audio/evolve_fanfare.mp3';
+  static const String audioCoin = 'assets/audio/coin_ding.mp3';
+  static const String audioTap = 'assets/audio/tap.mp3';
+
+  // ── Images (we use CustomPainter so no images needed) ──
+  static const String placeholder = 'assets/images/placeholder.png';
+}
