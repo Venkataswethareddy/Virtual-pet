4cbc4c0df08723e6f81770fe3800953d11e7fc65
Venkataswethareddy <swethasreenu555@gmail.com>
2023-01-16 10:30:00 +0530

    feat: add pet evolution type enum

---
+++ lib/core/enums/pet_type.dart
@@ -0,0 +1,11 @@
+/// The three evolution paths a pet can take based on care style.
+enum PetType {
+  /// Fed most → cat path
+  cat,
+  /// Played most → dog path
+  dog,
+  /// Slept most → bunny path
+  bunny,
+  /// Undecided – still in egg / baby stage
+  undecided,
+}
