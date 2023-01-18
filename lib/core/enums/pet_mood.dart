e569eca4231eb21bfea4e9839222e67da6b9fa4d
Venkataswethareddy <swethasreenu555@gmail.com>
2023-01-18 10:30:00 +0530

    feat: add pet mood and stage enums

---
+++ lib/core/enums/pet_mood.dart
@@ -0,0 +1,10 @@
+/// Mood is derived from the combination of all stats.
+enum PetMood {
+  happy,
+  sad,
+  hungry,
+  sick,
+  sleeping,
+  excited,
+  scared,
+}
---
+++ lib/core/enums/pet_stage.dart
@@ -0,0 +1,11 @@
+/// Evolution stages — progression is time-based.
+enum PetStage {
+  egg,
+  baby,
+  teen,
+  adult,
+  legendary,
+  /// Neglect sub-stages
+  sadForm,
+  ghostForm,
+}
