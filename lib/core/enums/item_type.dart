99f4f5ef94c448c6cc2d1d943d468706b25e5fb4
Venkataswethareddy <swethasreenu555@gmail.com>
2023-01-20 10:30:00 +0530

    feat: add personality trait and shop item type enums

---
+++ lib/core/enums/item_type.dart
@@ -0,0 +1,8 @@
+/// Categories of items in the shop / inventory.
+enum ItemType {
+  food,
+  toy,
+  medicine,
+  costume,
+  decoration,
+}
---
+++ lib/core/enums/personality_trait.dart
@@ -0,0 +1,7 @@
+/// Personality traits generated at birth.
+enum PersonalityTrait {
+  bravery,
+  activity,
+  friendliness,
+  appetite,
+}
